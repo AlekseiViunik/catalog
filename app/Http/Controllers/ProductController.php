ee7099aedec09bac2e2dbc81876edc0061b5a114
Aleksei Viunik <a79643739989@yandex.ru>
2024-09-05 17:55:43 +0200

    add a logic to ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = \App\Models\Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        $product = \App\Models\Product::find($request->product_id);
+        $quantity = $request->quantity;
+
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$product->id])) {
+            $cart[$product->id]['quantity'] += $quantity;
+        } else {
+            $cart[$product->id] = [
+                "name" => $product->name,
+                "quantity" => $quantity,
+                "price" => $product->price
+            ];
+        }
+
+        session()->put('cart', $cart);
+
+        return redirect()->route('products.index')->with('success', 'Product added to cart!');
+    }
 }
