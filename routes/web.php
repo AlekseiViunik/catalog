d674a1f3f817cc6bbd2a8357af26dfeb2efa3611
Aleksei Viunik <a79643739989@yandex.ru>
2024-09-05 18:14:09 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::post('/cart', [ProductController::class, 'addToCart'])->name('cart.add');
+Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
